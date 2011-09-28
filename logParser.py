07958235e47b83946a5712ee73e1fda150021e49
Celso Martins <celso.av.martins@gmail.com>
2011-09-28 16:49:50 -0300

    testes OK para o log parser - codigo mais conciso

--- logParser.py
+++ logParser.py
@@ -2,22 +2,18 @@
 # _*_ encoding: utf-8 _*_
 # logParser.py - le linhas interessantes de um determinado arquivo e escreve outro com este subconjunto
 
-arquivoEntrada = file("teste.txt")
 linhasInteressantes = list()
+
 cadeiaProcurada = raw_input("Escolha o padrao que deseja parsear: ")
+
+arquivoEntrada = file("teste.txt")
+nomeArquivoSaida = cadeiaProcurada.replace(" ", "_")
+arquivoSaida = file(nomeArquivoSaida + ".txt", "w")
+
 for linha in arquivoEntrada.readlines(): 
 	print linha,
 	if cadeiaProcurada in linha:
-		linhasInteressantes.append(linha)
+		arquivoSaida.write(linha)
 
 arquivoEntrada.close()
-
-nomeArquivoSaida = cadeiaProcurada.replace(" ", "_")
-
-arquivoSaida = file(nomeArquivoSaida + ".txt", "w")
-
-for linhaInteressante in linhasInteressantes:
-	arquivoSaida.write(linhaInteressante)
-
 arquivoSaida.close()
-
