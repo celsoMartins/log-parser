1867c83b52adf6975113875c800dd7fdd86ac2fc
Celso Martins <celso.av.martins@gmail.com>
2011-09-28 20:35:48 -0300

    alteracao para varrer todos os arquivos .log do diretorio atual

--- logParser.py
+++ logParser.py
@@ -2,16 +2,19 @@
 # _*_ encoding: utf-8 _*_
 # logParser.py - le linhas interessantes de um determinado arquivo e escreve outro com este subconjunto
 
-cadeiaProcurada = raw_input("Escolha o padrao que deseja parsear: ")
-arquivoDeBusca = raw_input("Nome do arquivo de busca: ")
+import os
+import glob
 
-arquivoEntrada = file(arquivoDeBusca)
+cadeiaProcurada = raw_input("Escolha o padrao que deseja parsear: ")
+
 nomeArquivoSaida = cadeiaProcurada.replace(" ", "_")
 arquivoSaida = file(nomeArquivoSaida + ".txt", "w")
 
-for linha in arquivoEntrada.readlines(): 
-	if cadeiaProcurada in linha:
-		arquivoSaida.write(linha)
+for nomeArquivoEntrada in glob.glob(os.path.join(".", "*.log*")):
+	arquivoEntrada = file(nomeArquivoEntrada)
+	for linha in arquivoEntrada.readlines(): 
+		if cadeiaProcurada in linha:
+			arquivoSaida.write(linha)
 
 arquivoEntrada.close()
 arquivoSaida.close()
